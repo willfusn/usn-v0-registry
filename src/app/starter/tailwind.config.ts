f94385691ef1fcc6ea16f0f3b7458762e84aea7d
willsather <will@sather.ws>
2025-05-02 15:30:40 -0400

    fix: tailwind config borderRadius

--- src/app/starter/tailwind.config.ts
+++ src/app/starter/tailwind.config.ts
@@ -89,13 +89,24 @@ const config: Config = {
         sans: "var(--font-sans)",
         serif: "var(--font-serif)",
         mono: "var(--font-mono)",
-        display: "var(--font-display)",
       },
 
       borderRadius: {
-        lg: "var(--radius)",
-        md: "var(--radius-md)",
         sm: "var(--radius-sm)",
+        md: "var(--radius-md)",
+        lg: "var(--radius-lg)",
+        xl: "var(--radius-xl)",
+      },
+
+      boxShadow: {
+        "2xs": "var(--shadow-2xs)",
+        xs: "var(--shadow-xs)",
+        sm: "var(--shadow-sm)",
+        DEFAULT: "var(--shadow)",
+        md: "var(--shadow-md)",
+        lg: "var(--shadow-lg)",
+        xl: "var(--shadow-xl)",
+        "2xl": "var(--shadow-2xl)",
       },
 
       keyframes: {
