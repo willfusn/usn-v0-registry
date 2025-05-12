ada4cf0709342c5a372e46eb0de2b06a495088f8
willsather <will@sather.ws>
2025-05-12 12:31:52 -0400

    fix: blank starter layout

--- src/app/starter/blank/root-layout.tsx
+++ src/app/starter/blank/root-layout.tsx
@@ -1,8 +1,6 @@
 import type React from "react";
 import { Toaster } from "sonner";
 
-import { BrandHeader } from "@/components/brand-header";
-import { BrandSidebar } from "@/components/brand-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 import "@/app/globals.css";
@@ -14,9 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <SidebarProvider>
-      <main className="mt-16 flex w-full justify-center">
-        <div className="container">{children}</div>
-      </main>
+      {children}
       <Toaster />
     </SidebarProvider>
   );
